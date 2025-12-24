6d751b62a30a691620358b41ce48ca63104cbbef
Gajendra <gajendranh1092@gmail.com>
2025-12-25 01:53:44 +0530

    Modified supabse to load env variable correctly

--- Backend/src/config/supabase.js
+++ Backend/src/config/supabase.js
@@ -1,8 +1,6 @@
+import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 export default supabase;
